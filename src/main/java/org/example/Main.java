78d07ee56fa7321cdff080495a6fe9328d550a96
unknown <sveta.myhailuk@gmail.com>
2023-05-02 17:00:41 +0100

    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,56 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many rounds would you like to play?");
+        int rounds = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < rounds; i++) {
+            playGame(scanner);
         }
     }
+
+    static void playGame(Scanner scanner) {
+        System.out.println("What move will you make?");
+        System.out.println("Rock");
+        System.out.println("Paper");
+        System.out.println("Scissors");
+        String playerMove = scanner.nextLine();
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(3);
+
+        String computerMove;
+        if (randomNumber == 0) {
+            computerMove = "Rock";
+        } else if (randomNumber == 1) {
+            computerMove = "Paper";
+        } else {
+            computerMove = "Scissors";
+        }
+        System.out.println("Computer chose " + computerMove + "!");
+
+        if (playerMove.equals(computerMove)) {
+            System.out.println("There is a draw!");
+        } else if (playerWins(playerMove, computerMove)) {
+            System.out.println("Player wins!");
+        } else {
+            System.out.println("Computer wins!");
+        }
+    }
+
+    static boolean playerWins(String playerMove, String computerMove) {
+        if (playerMove.equals("Rock")) {
+            return computerMove.equals("Scissors");
+        } else if (playerMove.equals("Paper")) {
+            return computerMove.equals("Rock");
+        } else {
+            return computerMove.equals("Paper");
+        }
+    }
+
 }
